670f831da0d2ceab1b5f888bd1c4a848aa1f5e77
Geralexan <g.lagos05@ufromail.cl>
2022-04-02 22:00:09 -0300

    commit German

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("German Alexander Lagos Llanos");
     }
 }
